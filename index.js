dd9980aa917a656ef3396a63a5a589c6eaa111c7
Amanda Arruda <amanda.reis.27112000@gmail.com>
2026-03-10 14:39:29 -0300

    Projeto Landing Page Arquitetura

---
+++ index.js
@@ -0,0 +1,10 @@
+// Seleciona o formulário
+const form = document.querySelector("form");
+
+// Quando o usuário enviar o formulário
+form.addEventListener("submit", function(event) {
+
+    // mensagem de confirmação
+    alert("Obrigado! Em breve entraremos em contato.");
+
+});
